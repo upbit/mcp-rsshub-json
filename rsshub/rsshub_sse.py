a0211b0e8ddbe292d158d557f51e3983bab5f658
Hao Zhou <rmusique@gmail.com>
2025-05-11 22:33:01 +0800

    fix: Add rsshub_sse.py for docker run

---
+++ rsshub/rsshub_sse.py
@@ -0,0 +1,23 @@
+from rsshub import *
+
+RSSHUB_INSTANCES = [
+    # "https://rsshub.app",
+    "https://rsshub.rssforever.com",
+    "https://rsshub.feeded.xyz",
+    "https://hub.slarker.me",
+    "https://rsshub.liumingye.cn",
+    "https://rsshub-instance.zeabur.app",
+    "https://rss.fatpandac.com",
+    "https://rsshub.pseudoyu.com",
+    "https://rsshub.friesport.ac.cn",
+    "https://rsshub.atgw.io",
+    "https://rsshub.rss.tips",
+    "https://rsshub.mubibai.com",
+    "https://rsshub.ktachibana.party",
+    "https://rsshub.woodland.cafe",
+    "https://rsshub.aierliz.xyz",
+    "http://localhost:1200",
+]
+
+if __name__ == "__main__":
+    mcp.run(transport="sse")
